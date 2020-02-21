95701a8530c8e3c93722f5ed8d61431703f64f03
Maggie Hollis <s013332@students.lmsd.org>
2020-02-21 13:27:08 -0500

    still firebase problems. view controller kinda working.

--- Tierodactyl/TierViewController.swift
+++ Tierodactyl/TierViewController.swift
@@ -8,45 +8,51 @@
 
 import UIKit
 
-class TierViewController: UIViewController{//, UICollectionViewDelegate, UICollectionViewDataSource {
+class TierViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 10
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//         let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath as IndexPath)
-//               myCell.backgroundColor = UIColor.blue
-//               return myCell
-//    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath as IndexPath)
+               myCell.backgroundColor = UIColor.blue
+               return myCell
+    }
     
     var first : UICollectionView?
     
     @IBAction func addView(_ sender: Any) {
         
     }
+    
+    func setUpView(_: UICollectionView?){
+       let layout = UICollectionViewFlowLayout()
+      // layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+       layout.itemSize = CGSize(width: 100, height: 100)
+       layout.scrollDirection = .horizontal
+        
+        first = UICollectionView(frame: self.view.frame, collectionViewLayout:  layout)
+        first!.delegate   = self
+        first!.dataSource = self
+        first!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        first!.backgroundColor = UIColor.white
+        
+        
+        first!.translatesAutoresizingMaskIntoConstraints = false
+        first!.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        first!.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        
+      
+        
+        view.addSubview(first!)
+    }
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      // let layout = UICollectionViewFlowLayout()
-//        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-//        layout.itemSize = CGSize(width: 111, height: 100)
-//        layout.scrollDirection = .horizontal
-        
-        first = UICollectionView(frame: self.view.frame, collectionViewLayout:  UICollectionViewFlowLayout())
-//        first.delegate   = self
-//        first.dataSource = self
- //       first.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
- //       first.backgroundColor = UIColor.white
-        
-        first!.translatesAutoresizingMaskIntoConstraints = false
-        first!.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        first!.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
-        first!.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 0).isActive = true
-        first!.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        view.addSubview(first!)
+        navigationController?.isNavigationBarHidden = false
+        setUpView(first)
         
     }
     
