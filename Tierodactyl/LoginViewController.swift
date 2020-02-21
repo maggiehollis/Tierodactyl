3aed5b15e80b1140192f34f510c7b56bfbe49af8
Sara Beqiraj <s013380@students.lmsd.org>
2020-02-21 12:25:25 -0500

    login time info is in

--- Tierodactyl/LoginViewController.swift
+++ Tierodactyl/LoginViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var username: UITextField!
+       
+       @IBOutlet weak var password: UITextField!
+       
+       @IBOutlet weak var loginButtonO: UIButton!
+       @IBAction func loginButton(_ sender: Any) {
+           guard let email = username.text else {return}
+           guard let password = password.text else {return}
+           Auth.auth().signIn(withEmail: email, password: password) { (user, error)
+               in
+               if let _ = user {
+                   self.dismiss(animated: true, completion: nil)
+               }
+               else {
+                   print(error!.localizedDescription)
+               }
+           }
+       }
+       func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           if username.isFirstResponder {
+               password.becomeFirstResponder()
+           }
+           else if password.isFirstResponder {
+               password.resignFirstResponder()
+               loginButtonO.isEnabled = true
+           }
+           return true
+       }
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+           // Do any additional setup after loading the view.
+       }
 
     /*
     // MARK: - Navigation
